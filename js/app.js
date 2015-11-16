6aa6746f546c070f1215c12a41c24908082c1996
furenku <furenku@gmail.com>
2015-11-16 03:03:56 -0600

    export to svg

--- js/app.js
+++ js/app.js
@@ -26,38 +26,79 @@ var wcs = [];
 
 window.onload = function() {
 
-	paper = new Raphael(document.getElementById('canvas'));
-
-	var text  = "Hello";
-
-	var words = text.split(" ");
-
-
-	for( wordIndex in words ) {
-
-		wordIndex = parseInt( wordIndex );
-
-		var word = words[wordIndex];
-
-		wc = new WordCircle();
-
-		wc.setPosition( Math.random() * 1000, Math.random() * 600 );
-		
-		wc.setText( word );
-
-		wcs.push( wc );
-		
-	}
-
-
+	
     	//sector( 100, 100, 30, 90, 135,{'stroke':'#fa0'})
 
-}	
+    }	
 
 
-setInterval(function(){
-	paper.clear();
-	for( i in wcs ) {
-		wcs[i].draw();
-	}
-}, 300)
+    setInterval(function(){
+    	paper.clear();
+    	for( i in wcs ) {
+    		wcs[i].draw();
+    	}
+    }, 300)
+
+
+	var textFile = null,
+	makeTextFile = function (text) {
+		var data = new Blob([text], {type: 'text/plain'});
+
+	    if (textFile !== null) {
+	    	window.URL.revokeObjectURL(textFile);
+	    }
+
+	    textFile = window.URL.createObjectURL(data);
+
+	    return textFile;
+	};
+
+    $(document).ready(function(){
+    	console.log("ready")
+    	$('#export').click(function(e){
+    		var svg = paper.toSVG();
+    		$('#download a').attr('target','_blank').attr('href',makeTextFile(svg)).removeClass('disabled');
+    		$('#download').removeClass('disabled');
+
+
+
+
+		var create = document.getElementById('create'),
+		textbox = document.getElementById('textbox');
+
+		// create.addEventListener('click', function () {
+		// 	var link = document.getElementById('downloadlink');
+		// 	link.href = makeTextFile(textbox.value);
+		// 	link.style.display = 'block';
+		// }, false);
+
+
+
+	})
+
+    	paper = new Raphael(document.getElementById('canvas'));
+
+    	var text  = "Hello Fucked Up World";
+
+    	var words = text.split(" ");
+
+
+    	for( wordIndex in words ) {
+
+    		wordIndex = parseInt( wordIndex );
+
+    		var word = words[wordIndex];
+
+    		wc = new WordCircle();
+
+    		wc.setPosition( Math.random() * 1000, Math.random() * 600 );
+
+    		wc.setText( word );
+
+    		wcs.push( wc );
+
+    	}
+
+
+
+    })
